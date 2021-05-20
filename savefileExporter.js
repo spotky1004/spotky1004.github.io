c4a8857843e9aade13a2b39116f616d6bba52a00
Spotky1004 <spotky1004@gmail.com>
2021-05-20 20:57:10 +0900

    Updated savefileExporter

--- savefileExporter.js
+++ savefileExporter.js
@@ -4,18 +4,33 @@ const toExport = {
     alphabetTree: "Alphabet Tree",
     ordinalDimSave: "Ordinal Dimensions",
     saveFile: "Universe Farm",
-    blockHoleShrinker: "Block Hole Shrinker (this game doesn't have export button, but here's your save)",
-
+    blockHoleShrinker: "Block Hole Shrinker (this game doesn't have export button, but here's your save)"
 };
 
+let saveString = "";
 const mainDiv = document.getElementById("main");
 for (const name in toExport) {
     if (localStorage[name]) {
+        let saveData = btoa(localStorage[name]);
+        try {
+            atob(localStorage[name]);
+            saveData = localStorage[name];
+        } catch (e) {}
+
         let namespace = document.createElement("div");
         namespace.innerText = toExport[name];
         mainDiv.append(namespace);
         let exportBox = document.createElement("textarea");
-        exportBox.value = btoa(localStorage[name]);
+        exportBox.value = saveData;
         mainDiv.append(exportBox);
+        
+        saveString += `localStorage.${name} = "${btoa(localStorage[name])}";`
     }
 }
+
+let namespace = document.createElement("div");
+namespace.innerHTML = "Copy this and paste at the Console(F12) of my new domain (<a href=\"https://spotky1004.com\">https://spotky1004.com</a>) to migrate";
+mainDiv.append(namespace);
+let exportBox = document.createElement("textarea");
+exportBox.value = saveString;
+mainDiv.append(exportBox);
